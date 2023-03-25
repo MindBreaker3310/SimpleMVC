9498f89d64a92d6319bf3ae97a20a7df43562b3e
MindBreaker3310 <32410092+MindBreaker3310@users.noreply.github.com>
2023-03-25 15:27:16 +0800

    init
    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SimpleMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
